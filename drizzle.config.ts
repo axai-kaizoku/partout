d4dfa4dae807c20174afc412b30e08a48be807f0
Akshay Yelle <02b3akshay@gmail.com>
2025-10-28 23:22:02 +0530

    add db config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/env";
 
 export default {
-	schema: "./src/server/db/schema.ts",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-	tablesFilter: ["partout_*"],
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.NEXT_PUBLIC_SUPABASE_URL,
+  },
+  tablesFilter: ["partout_*"],
 } satisfies Config;
---
+++ src/lib/supabase/middleware.ts
@@ -0,0 +1,32 @@
+import { env } from "@/env";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = (request: NextRequest) => {
+  // Create an unmodified response
+  let supabaseResponse = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(supabaseUrl!, supabaseKey!, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+        supabaseResponse = NextResponse.next({
+          request,
+        });
+        cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
+      },
+    },
+  });
+
+  return supabaseResponse;
+};
